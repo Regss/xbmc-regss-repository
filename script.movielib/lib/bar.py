84d6904d0bcbcfa42c430a7f79b1d09efeb55a67
Regss <regss84@gmail.com>
2014-09-18 13:37:51 +0200

    script.movielib 1.2.8

--- script.movielib/lib/bar.py
+++ script.movielib/lib/bar.py
@@ -16,19 +16,23 @@ sys.path.append(os.path.join(__addonpath__, "lib"))
 class Bar:
     def __init__(self):
         self.settingsBar  = __addon__.getSetting('bar')
-        self.b = xbmcgui.DialogProgressBG()
-    
+        self.xbmc_version = int(xbmc.getInfoLabel('System.BuildVersion')[0:2])
+        if self.xbmc_version > 12:
+            self.b = xbmcgui.DialogProgressBG()
+        else:
+            self.b = xbmcgui.DialogProgress()
+            
     def create(self, message, heading):
         if 'true' in self.settingsBar:
             self.b.create(heading, message)
-    
-    def update(self, percent, message, heading=0):
+            
+    def update(self, percent, message):
         if 'true' in self.settingsBar:
-            if heading == 0:
+            if self.xbmc_version > 12:
                 self.b.update(percent, message=message)
             else:
-                self.b.update(percent, heading, message)
-        
+                self.b.update(percent, line1=message)
+                
     def close(self):
         if 'true' in self.settingsBar:
             self.b.close()
